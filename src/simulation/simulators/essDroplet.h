7608617fec17d4206302e8ae09ed3cdf4bcb61bd
micheltakken <micheltakken@gmail.com>
2024-08-05 16:32:44 +0200

    :construction: Add Hybrid Droplet Simulator for essLbm

---
+++ src/simulation/simulators/essDroplet.h
@@ -0,0 +1,95 @@
+/**
+ * @file essContinuous.h
+*/
+
+#pragma once
+
+#define M_PI 3.14159265358979323846
+
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <math.h>
+#include <iostream>
+
+#include "Module.h"
+#include "ModuleOpening.h"
+#include "Node.h"
+#include "Channel.h"
+
+#include <ESSDropletSolver.h>
+
+namespace arch {
+
+// Forward declared dependencies
+template<typename T>
+class Module;
+template<typename T>
+class Network;
+template<typename T>
+class Node;
+template<typename T>
+class Opening;
+
+}
+
+namespace sim {
+
+    /**
+     * @brief Class that defines the lbm module which is the interface between the 1D solver and OLB.
+    */
+    template<typename T>
+    class essLbmDropletSimulator : public essLbmSimulator<T> {
+        private:
+
+            std::unordered_map<int, T> bufferZones;                 ///< For each opening we need a buffer zone to generate droplet <nodeId, bufferLength>
+            std::unordered_map<int, sim::Droplet<T>*> lbmDroplets;  ///< Map of droplets in the lbm zone <dropletId, dropletPointer>
+
+            std::shared_ptr<ess::lbmSolver> solver_;
+
+        public:
+            /**
+             * @brief Constructor of an lbm module.
+             * @param[in] id Id of the module.
+             * @param[in] name Name of the module.
+             * @param[in] pos Absolute position of the module in _m_, from the bottom left corner of the microfluidic device.
+             * @param[in] size Size of the module in _m_.
+             * @param[in] nodes Map of nodes that are on the boundary of the module.
+             * @param[in] openings Map of the in-/outlets of the module.
+            */
+            essLbmDropletSimulator(int id_, std::string name_, std::string stlFile_, std::shared_ptr<arch::Module<T>> cfdModule,  std::unordered_map<int, arch::Opening<T>> openings_,
+                            ResistanceModel<T>* resistanceModel, T charPhysLength_, T charPhysVelocity_, T alpha, T resolution_, T epsilon_, T relaxationTime_);
+            /**
+             * @brief Initialize an instance of the LBM solver for this module.
+             * @param[in] dynViscosity Dynamic viscosity of the simulated fluid in _kg / m s_.
+             * @param[in] density Density of the simulated fluid in _kg / m^3_.
+            */
+            void lbmInit(T dynViscosity, T density) override;
+
+            /**
+             * @brief Generate a droplet in the LBM solver
+             * @return Id of the generated droplet
+             */
+            int generateDroplet(sim::Droplet<T>* droplet, Node<T>* entrypoint);
+
+            /**
+             * @brief Purge the droplet with Id in the buffer zone from the LBM solver
+             * @return Opening id, where the droplet exits LBM domain.
+             */
+            int purgeDroplet(int dropletId);
+
+            /**
+             * @brief Create a droplet in the 1D solver, that is exiting the CFD domain.
+             * @param[in] Id 
+             * @param[in] volume 
+             * @param[in] Fluid
+             * @return Droplet instance
+             */
+            int addDroplet(T dropletVolume, T viscosity, T density, T timeStamp, int channelId, T position);
+
+            /**
+             * @brief Removes a droplet in the 1D solver, that got consumed in a merge operation
+             */
+            void removeDroplet(int dropletId);
+    };
+}
---
+++ src/simulation/simulators/essDroplet.hh
@@ -0,0 +1,102 @@
+#include "essContinuous.h"
+#include <mpi.h>
+
+#include <iostream>
+#include <type_traits>
+
+namespace sim{
+
+    template<typename T>
+    essLbmDropletSimulator<T>::essLbmDropletSimulator(int id_, std::string name_, std::string stlFile_, std::shared_ptr<arch::Module<T>> cfdModule_,  std::unordered_map<int, arch::Opening<T>> openings_,
+                                ResistanceModel<T>* resistanceModel_, T charPhysLength_, T charPhysVelocity_, T alpha_, T resolution_, T epsilon_, T relaxationTime_) :
+            essLbmSimulator<T>(id_, name_, stlFile_, cfdModule_, openings_, resistanceModel_, charPhysLength_, charPhysVelocity_, alpha_, resolution_, epsilon_, relaxationTime_)
+    {
+        this->cfdModule->setModuleTypeEssLbm();
+    }
+
+    template<typename T>
+    void essLbmSimulator<T>::lbmInit(T dynViscosity, T density)
+    {
+
+        std::string work_dir = "/home/michel/Git/mmft-hybrid-simulator/build/";
+        const auto& allNodes = this->moduleNetwork->getNodes();
+        std::unordered_map<int, ess::BoundaryNode> nodes(allNodes.size());
+        std::unordered_map<int, ess::Opening> openings;
+
+        for(auto& op : this->moduleOpenings)
+        {
+            ess::Opening opening;
+            opening.width = op.second.width;
+            opening.height = op.second.height;
+            opening.normal = {op.second.normal[0],op.second.normal[1],op.second.normal[2]};
+            openings.try_emplace(op.first, opening);
+
+            ess::BoundaryNode node(allNodes.at(op.first)->getPosition()[0],
+                                   allNodes.at(op.first)->getPosition()[1],
+                                   allNodes.at(op.first)->getPosition()[2],
+                                   allNodes.at(op.first)->getGround());
+
+            nodes.try_emplace(op.first, node);
+        }
+
+        solver_ = std::make_shared<ess::lbmSolver>(work_dir, this->stlFile, openings, nodes, charPhysLength, charPhysVelocity, 1.0f / resolution, epsilon, relaxationTime, density, dynViscosity);
+        solver_->prepareLattice();
+
+        #ifdef DEBUG
+        // There must be more than 1 node to have meaningful flow in the module domain
+        assert(this->moduleOpenings.size() > 1);
+        // We must have exactly one opening assigned to each boundaryNode
+        assert(this->moduleOpenings.size() == this->cfdModule->getNodes().size());
+        #endif
+
+        // Initialize pressure, flowRate and resistance value-containers
+        for (auto& [key, node] : this->moduleOpenings)
+        {
+            pressures.try_emplace(key, 0.0f);
+            flowRates.try_emplace(key, 0.0f);
+        }
+
+        setFlowRates(flowRates);
+        setPressures(pressures);
+
+        #ifdef VERBOSE
+            std::cout << "[essLbmModule] lbmInit " << this->name << "... OK" << std::endl;
+        #endif
+    }
+
+    template<typename T>
+    int essLbmSimulator<T>::generateDroplet(sim::Droplet<T>* droplet, Node<T>* entrypoint) {
+        int dropletId = 1;
+        std::vector<T> nodePosition = (0.0, 0.0, 0.0);
+        T density = 1.0;
+        T viscosity = 1.0;
+        T volume = 1.0;
+        this->solver_->generateDroplet(dropletId, nodePosition, density, viscosity, volume);
+        droplet->setDropletState(sim::DropletState::IDLE);
+        lbmDroplets.try_emplace(dropletId, nullptr);
+    }
+
+    template<typename T>
+    int essLbmSimulator<T>::purgeDroplet(int dropletId) {
+        dropletId = 1;
+        assert(lbmDroplets.count(dropletId));
+        lbmDroplets.at(dropletId)->setDropletState(sim::DropletState::NETWORK);
+        lbmDroplets.erase(dropletId);
+    }
+
+    template<typename T>
+    int essLbmSimulator<T>::addDroplet(int parentDropletId, T volumeParentDroplet, T volumeChildDroplet) {
+        int dropletId = 2;
+        lbmDroplets.try_emplace(dropletId, nullptr);
+        // TODO: Add child droplet to droplet map in simulation object
+        return dropletId;
+    }
+
+    template<typename T>
+    void essLbmSimulator<T>::removeDroplet(int dropletId) {
+        assert(lbmDroplets.count(dropletId));
+        // TODO: Remove droplet from droplet map in simulation object
+        lbmDroplets.erase(dropletId);
+    }
+
+}   // namespace arch
